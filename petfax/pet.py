edc12e765487193f74d110bee737247fe36d5b93
Werefox <jack@wrightus.com>
2022-08-29 18:52:04 -0400

    Added show route and template

--- petfax/pet.py
+++ petfax/pet.py
@@ -2,10 +2,16 @@ from flask import Blueprint, render_template
 import json
 
 pets = json.load(open("pets.json"))
-print(pets)
 
 bp = Blueprint('pet', __name__, url_prefix='/pets')
 
 @bp.route('/')
 def index():
 	return render_template('index.html', pets=pets)
+
+@bp.route('/<int:id>')
+def show(id):
+	if id >= 0 and id < len(pets):
+		return render_template('pet_show.html', pet=pets[id])
+	else:
+		return '404: Sorry, that index is invalid.'
